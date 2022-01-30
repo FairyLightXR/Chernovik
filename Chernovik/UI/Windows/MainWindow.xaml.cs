2962854f99c44d5807f88d7182e1b99c31ab0902
FairyLightXR <gundorovsasha@gmail.com>
2022-01-30 14:41:54 +0300

    Created entity model; Designed MainWindow; Added Styles; Added Transition class

--- Chernovik/UI/Windows/MainWindow.xaml.cs
+++ Chernovik/UI/Windows/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Chernovik.Entities;
+using Chernovik.UI.Pages;
+using Chernovik.Utilities;
+
 
 namespace Chernovik
 {
@@ -20,9 +24,41 @@ namespace Chernovik
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Осуществление перехода на страницу материалов
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new MaterialLV());
+            Transition.MainFrame = MainFrame;
+        }
+
+        /// <summary>
+        /// Код видимости кнопки назад на главной форме
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if(Transition.MainFrame.CanGoBack)
+            {
+                BackBtn.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                BackBtn.Visibility= Visibility.Visible;
+            }
+        }
+
+        /// <summary>
+        /// Код кнопки назад
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Transition.MainFrame.GoBack();
         }
     }
 }
---
+++ Chernovik/Utilities/Transition.cs
@@ -0,0 +1,27 @@
+﻿using Chernovik.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Chernovik.Utilities
+{
+    internal class Transition
+    {
+        public static Frame MainFrame { get; set; }
+
+        private static ChernovikDBEntities datacontext;
+        public static ChernovikDBEntities DataContext
+        {
+            get
+            {
+                if (datacontext == null)
+                    datacontext = new ChernovikDBEntities();
+
+                return datacontext;
+            }
+        }
+    }
+}
